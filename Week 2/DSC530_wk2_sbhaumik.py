804cdaa5357047539893bd1dbf30b88577e4aa66
Siddhartha Bhaumik <38511681+sidbhaumik@users.noreply.github.com>
2022-06-19 17:01:59 -0400

    Add files via upload

---
+++ Week 2/DSC530_wk2_sbhaumik.py
@@ -0,0 +1,53 @@
+"""
+Program Details:
+
+Course: DSC 530
+Version#:1: Week 2, Programming Assignment 2.1
+
+Requirements:
+Display the text “Hello World!
+Add two numbers together
+Subtract a number from another number
+Multiply two numbers
+Divide between two numbers
+Concatenate two strings together (any words)
+Create a list of 4 items (can be strings, numbers, both)
+Append an item to your list (again, can be a string, number)
+Create a tuple with 4 items (can be strings, numbers, both)
+
+Author: Siddhartha Bhaumik
+Release Date: 06/18/2022
+"""
+
+# Print Hello World!
+print('Hello World!')
+
+# Add two numbers together
+print(11 + 12)
+
+# Subtract a number from another number
+print(11 - 12)
+
+# Multiply two numbers
+print(11 * 12)
+
+# Divide between two numbers
+print(11 / 12)
+
+# Concatenate two strings together (any words)
+print('Hello' + ' World')
+
+# Create a list of 4 items (can be strings, numbers, both)
+my_list = ["Bellevue", "University", "Winter", "Class"]
+print(my_list)
+
+# Append an item to your list (again, can be a string, number)
+my_list.append(2021)
+print(my_list)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+my_tuple = ("Data", "Science", "AI", "Machine Learning")
+print(my_tuple)
+
+
+
